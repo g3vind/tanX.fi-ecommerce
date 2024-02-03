9f7e666bc1a27bd95347cf33aaeac737c7f7355a
g3vind <g3vind@gmail.com>
2024-02-03 15:57:50 +0530

    minor fixes

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 const EmptyCart = () => {
   return (
     <div className="flex flex-col justify-center items-center">
+      <h2 className="text-4xl text-red-500 font-bold mt-4">Cart is Empty!</h2>
       <img
         src="https://cdn-icons-png.flaticon.com/512/102/102661.png"
         alt="empty-cart-icon"
-        className="h-[340px] mt-12 w-auto"
+        className="h-[300px] mt-6 w-auto"
       />
       <Link to="/">
         <button className="w-40 h-12 rounded-xl text-white bg-blue-500 mt-20">
--- src/components/EmptyFavorites.js
+++ src/components/EmptyFavorites.js
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 const EmptyFavorites = () => {
   return (
     <div className="flex flex-col justify-center items-center">
+      <h2 className="text-4xl text-red-500 font-bold mt-4">
+        No Favorite Items
+      </h2>
       <img
         src="https://static.thenounproject.com/png/152176-200.png"
         alt="empty-cart-icon"
-        className="h-[340px] mt-12 w-auto"
+        className="h-[340px] mt-8 w-auto"
       />
       <Link to="/">
         <button className="w-40 h-12 -ml-16 rounded-xl text-white bg-green-500 mt-20">
